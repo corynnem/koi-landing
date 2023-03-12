e10290f5fee0c0b1cebdb4e78391696288891abd
Corynne Moody <moody.corynne@gmail.com>
2023-03-12 00:32:21 -0500

    storing for later

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,32 +1,93 @@
-import React, { useEffect } from 'react'
-import koi from '../public/images/koi.jpeg'
-import logo from '../public/images/logo.png'
-import $ from 'jquery'
+import React, { useEffect } from "react";
+import koi from "../public/images/koi.jpeg";
+import logo from "../public/images/logo.png";
+import $ from "jquery";
 
 const Welcome = () => {
+  useEffect(() => {
+    $(document).ready(() => {
+      $("#ripples").ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.04,
+      });
+      screen;
+    });
+  }, []);
 
-
-
-
-    // useEffect(() => {
-
-    // $('#ripples').ripples({
-    //     resolution: 512,
-    //     dropRadius: 20,
-    //     perturbance: 0.04,
-    //   });
-    // }, [])
-
-    return (
-        <div id="ripples">
-                
-                <h1 style={{fontSize: '80px', height: '80px', position: 'relative', top: -30, zIndex: 1000, color: 'white', marginLeft: '50px' }} >
-                    K<img src={logo.src} style={{width: '80px', marginBottom: '-10px', position: 'relative', zIndex: 1000}}/>i
-                </h1>
-                 <img src={koi.src} style={{width: '100vw', zIndex: '-1000', position: 'relative', marginTop: '-225px'}}/>
-            
-        </div>
-    )
-}
+  return (
+    <div id="ripples">
+      {/* {windowPresent && (
+        <WaterWave
+          imageUrl={koi.src}
+          // style={{
+          //   width: "100vw",
+          //   zIndex: "-1000",
+          //   position: "relative",
+          //   marginTop: "-225px",
+          // }}
+        >
+          {(methods) => (
+            <h1
+              style={{
+                fontSize: "80px",
+                height: "80px",
+                position: "relative",
+                top: -30,
+                zIndex: 1000,
+                color: "white",
+                marginLeft: "50px",
+              }}
+            >
+              K
+              <img
+                src={logo.src}
+                style={{
+                  width: "80px",
+                  marginBottom: "-10px",
+                  position: "relative",
+                  zIndex: 1000,
+                }}
+              />
+              i
+            </h1>
+          )}
+        </WaterWave>
+      )} */}
+      <h1
+        style={{
+          fontSize: "80px",
+          height: "80px",
+          position: "relative",
+          top: -30,
+          zIndex: 1000,
+          color: "white",
+          marginLeft: "50px",
+        }}
+      >
+        K
+        <img
+          src={logo.src}
+          style={{
+            width: "80px",
+            marginBottom: "-10px",
+            position: "relative",
+            zIndex: 1000,
+          }}
+        />
+        i
+      </h1>
+      <img
+        src={koi.src}
+        style={{
+          width: "100vw",
+          zIndex: "-1000",
+          position: "relative",
+          marginTop: "-225px",
+        }}
+      />
+    </div>
+  );
+};
 
 export default Welcome;
