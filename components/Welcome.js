53bcf5b7c0fca83cf4568c9c8e76cc110b2e6664
Corynne Moody <moody.corynne@gmail.com>
2023-03-21 19:52:55 -0400

    Update to merge

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,29 +4,53 @@ import logo from "../public/images/logo.png";
 import $ from "jquery";
 let ripples = import("jquery.ripples");
 
-const Welcome = () => 
-    useEffect(() => {
+const Welcome = () => {
+  useEffect(() => {
+    $("#ripples").ripples({
+      resolution: 600,
+      dropRadius: 27,
+      perturbance: 0.05,
+    });
+  }, []);
 
-    $('#ripples').ripples({
-        resolution: 600,
-        dropRadius: 27,
-        perturbance: 0.05,
-      });
-    }, [])
-
-    return (
-        <div>
-        
-                <h1 style={{fontSize: '80px', height: '80px', position: 'relative', top: -30, zIndex: 1000, color: 'white', marginLeft: '50px' }} >
-                    K<img src={logo.src} style={{width: '80px', marginBottom: '-10px', position: 'relative', zIndex: 1000}}/>i
-                </h1>
-                <div id="ripples"  style={{width: '100vw',height: '50vw', position: 'relative',marginTop: '-225px', backgroundImage: `url(${koi.src})`}} >
-        </div> 
-                 {/* <img src={koi.src} style={{width: '100vw', zIndex: '-1000', position: 'relative', marginTop: '-225px'}}/> */}
-            
-        
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1
+        style={{
+          fontSize: "80px",
+          height: "80px",
+          position: "relative",
+          top: -30,
+          zIndex: 1000,
+          color: "white",
+          marginLeft: "50px",
+        }}
+      >
+        K
+        <img
+          src={logo.src}
+          style={{
+            width: "80px",
+            marginBottom: "-10px",
+            position: "relative",
+            zIndex: 1000,
+          }}
+        />
+        i
+      </h1>
+      <div
+        id="ripples"
+        style={{
+          width: "100vw",
+          height: "50vw",
+          position: "relative",
+          marginTop: "-225px",
+          backgroundImage: `url(${koi.src})`,
+        }}
+      ></div>
+      {/* <img src={koi.src} style={{width: '100vw', zIndex: '-1000', position: 'relative', marginTop: '-225px'}}/> */}
+    </div>
+  );
+};
 
 export default Welcome;
