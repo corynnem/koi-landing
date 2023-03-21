bba6aec1f8c37117016e9784be4aaea2249b332d
corynnem <61360150+corynnem@users.noreply.github.com>
2023-03-21 19:41:20 -0400

    Update Welcome.js

--- components/Welcome.js
+++ components/Welcome.js
@@ -20,7 +20,6 @@ const Welcome = () => {
 
     return (
         <div>
-        
                 <h1 style={{fontSize: '80px', height: '80px', position: 'relative', top: -30, zIndex: 1000, color: 'white', marginLeft: '50px' }} >
                     K<img src={logo.src} style={{width: '80px', marginBottom: '-10px', position: 'relative', zIndex: 1000}}/>i
                 </h1>
