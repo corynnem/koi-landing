60a060c47a04d9ef2e396e8cc7bd71ca92a0e54e
Corynne Moody <moody.corynne@gmail.com>
2023-04-10 22:00:57 -0400

    fixed A

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+import About from "components/AboutUs";
+import Navbar from "components/Navbar";
+
+const AboutUsPage = () => {
+  return <div></div>;
+};
+
+export default AboutUsPage;
