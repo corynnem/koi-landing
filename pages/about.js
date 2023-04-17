f365f7b4829f16fa4a9f6d5208e17e70d9f697b3
Corynne Moody <moody.corynne@gmail.com>
2023-04-16 20:42:39 -0400

    add abt page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,23 @@
-import React from "react";
-import About from "components/AboutUs";
+import React, { useEffect } from "react";
+
 import Navbar from "components/Navbar";
+import LetterA from "../public/images/letters/A1.png";
+import LetterB from "../public/images/letters/b1.jpg";
+import LetterO from "../public/images/letters/o1.jpg";
+import LetterU from "../public/images/letters/U1.png";
+import LetterT from "../public/images/letters/T1.png";
+import LetterU2 from "../public/images/letters/U2.png";
+import LetterS from "../public/images/letters/UpperS.png";
+import paperBackdrop from "../public/images/paper_backdrop.jpg";
+import { letterJumble } from "../components/custom";
+import LoveHands from "../public/images/love_hands.png";
+import SupportUs from "components/SupportUs";
+import woodenFigure from "../public/images/fig_1.png";
 
 const AboutUsPage = () => {
+  useEffect(() => {
+    letterJumble();
+  }, []);
   return (
     <div>
       <Navbar />
@@ -10,47 +25,88 @@ const AboutUsPage = () => {
         style={{
           position: "absolute",
           marginTop: "100px",
-          paddingLeft: "10%",
-          paddingRight: "10%",
+          paddingLeft: "5%",
+          paddingRight: "5%",
         }}
       >
-        <h1 style={{ textAlign: "center" }}> About Koi</h1>
-        <p style={{ paddingLeft: "10%", paddingRight: "10%" }}>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              gap: "10px",
+              justifyContent: "center",
+            }}
+          >
+            <div>
+              <img src={LetterA.src} className=" outline large" />
+              <img src={LetterB.src} className=" outline medium" />
+              <img src={LetterO.src} className=" outline medium" />
+              <img src={LetterU.src} className=" outline medium" />
+              <img src={LetterT.src} className=" outline medium" />
+            </div>
+
+            <div>
+              <img src={LetterU2.src} className=" outline medium" />
+              <img src={LetterS.src} className=" outline medium" />
+            </div>
+          </div>
+        </div>
+        <p
+          style={{
+            paddingLeft: "10%",
+            paddingRight: "10%",
+            marginTop: "-40px",
+          }}
+        >
           <br />
-          Looking for love? Tired of swiping through impersonal profiles on
-          corporate-owned dating apps? Look no further! Koi is all about finding
-          love without any restrictions or limitations. We believe that finding
-          love should be free, and everyone should have the opportunity to find
-          their soulmate without any financial barriers. That's why our app is
-          Koi is independently owned and operated, with no influence from big
-          corporations. We put you in control of your dating experience, so you
-          can connect with people who truly matter to you.
-          <br />
-          <br />
-          At our dating app, everyone is welcome regardless of age, sexuality,
-          or gender. We believe that love is love, and it knows no boundaries.
-          Our app is inclusive, diverse, and welcoming to
-          <b> everyone</b>.
-          <br />
-          <br />
-          We also believe that dating should be fun! That's why we've designed
-          our app to be engaging, interactive, and exciting. You'll have a blast
-          swiping through profiles, sending messages, and making meaningful
-          connections with like-minded people. Our app is not just another
-          corporate-owned dating app that feels mechanical, disconnected, and
-          impersonal. We're a community-driven platform that values human
-          connection and real relationships.
-          <br />
-          <br />
-          We're committed to creating a dating experience that's personal,
-          authentic, and meaningful. And most importantly, we will not sell out
-          to large corporations. We respect your privacy and your personal data,
-          and we'll never compromise on that. We're here to build a better
-          dating app that balances the playing field between genders and
-          promotes healthy, happy relationships. So, what are you waiting for?
-          Download our dating app today and start your journey to finding love.
-          We promise it'll be an experience like no other!
+          <img
+            src={woodenFigure.src}
+            style={{
+              marginLeft: "-50px",
+              width: "150px",
+              height: "auto",
+            }}
+          />
+          <div
+            style={{
+              backgroundImage: `url(${paperBackdrop.src})`,
+              padding: "50px",
+              marginTop: "-110px",
+            }}
+          >
+            Looking for love? Tired of swiping through impersonal profiles on
+            corporate-owned dating apps? Look no further! Koi is all about
+            finding love without any restrictions or limitations. That's why our
+            app is Koi is independently owned and operated, with no influence
+            from big corporations. We put you in control of your dating
+            experience, so you can connect with people who truly matter to you.
+            <br />
+            <br />
+            At our dating app, everyone is welcome regardless of age, sexuality,
+            or gender. We believe that love is love, and it knows no boundaries.
+            Our app is inclusive, diverse, and welcoming to
+            <b> everyone</b>.
+            <br />
+            <br />
+            We also believe that dating should be fun! That's why we've designed
+            Koi to be engaging, interactive, and exciting. Our app is not just
+            another corporate-owned dating app that feels mechanical,
+            disconnected, and impersonal. We're a community-driven platform that
+            values human connection and real relationships.
+            <br />
+            <br />
+            Most importantly, we will not sell out to large corporations. We
+            respect your privacy and your personal data, and we'll never
+            compromise on that. We're here to build a better dating app that
+            balances the playing field between genders and promotes healthy,
+            happy relationships. So, what are you waiting for? Download our
+            dating app today and start your journey to finding love. We promise
+            it'll be an experience like no other!
+          </div>
         </p>
+        <SupportUs />
       </div>
     </div>
   );
