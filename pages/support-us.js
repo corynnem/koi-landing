e9df4efb4d64ff0507660ac89c798c007a477753
Corynne Moody <moody.corynne@gmail.com>
2023-04-23 19:15:53 -0400

    add navs

--- pages/support-us.js
+++ pages/support-us.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "components/Navbar";
 
 const SupportUsPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default SupportUsPage;
