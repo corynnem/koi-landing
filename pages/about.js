db19e628a2f192f04842e4b14bcbf8f869e44237
Corynne Moody <moody.corynne@gmail.com>
2023-04-23 16:31:53 -0400

    + apos

--- pages/about.js
+++ pages/about.js
@@ -75,10 +75,11 @@ const AboutUsPage = () => {
           >
             Looking for love? Tired of swiping through impersonal profiles on
             corporate-owned dating apps? Look no further! Koi is all about
-            finding love without any restrictions or limitations. That's why our
-            app is Koi is independently owned and operated, with no influence
-            from big corporations. We put you in control of your dating
-            experience, so you can connect with people who truly matter to you.
+            finding love without any restrictions or limitations. That&apos;s
+            why our app is Koi is independently owned and operated, with no
+            influence from big corporations. We put you in control of your
+            dating experience, so you can connect with people who truly matter
+            to you.
             {/* <br />
             <br /> */}
             At our dating app, everyone is welcome regardless of age, sexuality,
@@ -87,20 +88,21 @@ const AboutUsPage = () => {
             {/* <b> everyone</b>. */}
             {/* <br />
             <br /> */}
-            We also believe that dating should be fun! That's why we've designed
-            Koi to be engaging, interactive, and exciting. Our app is not just
-            another corporate-owned dating app that feels mechanical,
-            disconnected, and impersonal. We're a community-driven platform that
-            values human connection and real relationships.
+            We also believe that dating should be fun! Tha&apos;'s why
+            we&apos;ve designed Koi to be engaging, interactive, and exciting.
+            Our app is not just another corporate-owned dating app that feels
+            mechanical, disconnected, and impersonal. We&apos;re a
+            community-driven platform that values human connection and real
+            relationships.
             {/* <br />
             <br /> */}
             Most importantly, we will not sell out to large corporations. We
-            respect your privacy and your personal data, and we'll never
-            compromise on that. We're here to build a better dating app that
-            balances the playing field between genders and promotes healthy,
-            happy relationships. So, what are you waiting for? Download our
-            dating app today and start your journey to finding love. We promise
-            it'll be an experience like no other!
+            respect your privacy and your personal data, and we&apos;ll never
+            compromise on that. We&apos;re here to build a better dating app
+            that balances the playing field between genders and promotes
+            healthy, happy relationships. So, what are you waiting for? Download
+            our dating app today and start your journey to finding love. We
+            promise it&apos;ll be an experience like no other!
           </p>
         </div>
         <SupportUs />
