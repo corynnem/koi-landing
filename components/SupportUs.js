c114b3b9a17139eb58946e75c5324984c58a4a9f
Corynne Moody <moody.corynne@gmail.com>
2023-05-06 17:36:03 -0400

    rm other support page

--- components/SupportUs.js
+++ components/SupportUs.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const SupportUs = () => {
-  return <div></div>;
+  const iconStyles = {
+    height: "80px",
+    width: "80px",
+  };
+  return (
+    <div
+      className=" dot-border-yellow"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        marginTop: "200px",
+        paddingTop: "50px",
+        paddingBottom: "100px",
+        gap: "50px",
+        marginBottom: "100px",
+        backgroundImage: ` url(${bubbleWrap.src})`,
+      }}
+    >
+      <h1>Support Us</h1>
+      <div style={{ display: "flex", justifyContent: "center", gap: "40px" }}>
+        <img src={tikTok.src} style={iconStyles} />
+        <img src={patreon.src} style={iconStyles} />
+      </div>
+    </div>
+  );
 };
 
 export default SupportUs;
